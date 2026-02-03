0072487794f982076ddcf807e5d19006d1fc1646
ZAGO JEAN EZECHIEL <zagojean225@gmail.com>
2026-02-03 09:06:14 +0000

    mise a jour

--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex h-screen *:h-full">
       {/* side bar */}
 
-      <nav className="w-70  text-white flex flex-col items-center gap-20">
+      <nav className="w-70   flex flex-col items-center gap-14">
         <div className="w-full flex items-center justify-center pt-5">
           <div className="w-55 h-17  overflow-hidden  relative ">
             <Image
@@ -17,25 +17,28 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             />
           </div>
         </div>
+
         <div className=" h-0.5 flex items-center justify-center ">
           <Link
             href="/tableau-de-bord"
-            className="font-bold  text-black bg-orange-300   rounded-lg  py-4 px-12 flex items-center  gap-3"
+            className=" text-black hover:text-white  bg-white hover:bg-orange-500   rounded-lg  py-4 px-12 flex items-center  gap-3"
           >
-            <Image
-              src="/dashboard.png"
-              alt="Picture of the author"
-              width={10}
-              height={10}
-              className="h-5 w-5 object-cover "
-            />
+            <div className="w-fit bg-amber-500">
+              <Image
+                src="/dashboard.png"
+                alt="Picture of the author"
+                width={10}
+                height={10}
+                className="h-5 w-5 object-cover "
+              />
+            </div>
             Dashboard
           </Link>
         </div>
         <div className=" h-0.5 flex items-center justify-center">
           <Link
-            href="/tableau-de-bord"
-            className="font-bold  text-black   rounded-lg px-12 py-4 flex items-center  gap-3"
+            href="/campagne"
+            className=" text-black hover:text-white  bg-white hover:bg-orange-500    rounded-lg px-12 py-4 flex items-center  gap-3"
           >
             <Image
               src="/campagne.png"
@@ -44,13 +47,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
               height={10}
               className="h-5 w-5 object-cover  text-white"
             />
-            Campagne
+            Campagnes
           </Link>
         </div>
         <div className=" h-0.5 flex items-center justify-center ">
           <Link
             href="/employees"
-            className="font-bold  text-black   rounded-lg px-12 py-4 flex items-center  gap-3"
+            className="  text-black hover:text-white  bg-white hover:bg-orange-500    rounded-lg px-12 py-4 flex items-center  gap-3"
           >
             <Image
               src="/emp.png"
@@ -65,7 +68,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className=" h-0.5 flex items-center justify-center ">
           <Link
             href="/tache"
-            className="font-bold text-black    rounded-lg px-17 py-4 flex items-center  gap-3"
+            className=" text-black hover:text-white    bg-white hover:bg-orange-500   rounded-lg px-13 py-4 flex items-center  gap-3"
           >
             <Image
               src="/tache.png"
@@ -74,14 +77,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
               height={10}
               className="h-5 w-5 object-cover  text-white"
             />
-            tâche
+            Mes tâche
           </Link>
         </div>
       </nav>
       {/* side bar */}
       <main className="w-full h-screen flex flex-col">
         <div className="w-full bg-white h-17">header</div>
-        <div className="bg-gray-300 flex-1">{children}</div>
+        <div className="bg-gray-100 flex-1 p-7">
+          {/* <div className="w-full h-15 bg-green-700 rounded-md">gnjgnlk</div> */}
+
+          {children}
+        </div>
       </main>
     </div>
   );
--- src/app/(core)/tableau-de-bord/page.tsx
+++ src/app/(core)/tableau-de-bord/page.tsx
@@ -1,5 +1,7 @@
-export default function Page(){
-    return(
-        <div className="text-orange-400 text-2xl">tableau de bord</div>
-    )
+export default function Page() {
+  return (
+    <div className="text-orange-400 text-2xl">
+      <div className="w-full h-15 bg-green-700 rounded-md">tableau de bord</div>
+    </div>
+  );
 }
